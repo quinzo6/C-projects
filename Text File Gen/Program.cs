da7285c8ae6b639e667bdce9e869fe2a9b64d7a5
kkcoder69 <62224705+kkcoder69@users.noreply.github.com>
2020-11-18 15:11:34 -0500

    Initial Commit

---
+++ Text File Gen/Program.cs
@@ -0,0 +1,26 @@
+﻿using System;
+using System.Collections.Generic;
+using System.IO;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Text_File_Gen
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            try {
+                TextWriter tw = new StreamWriter("C:/FuckMyAsshole.txt", true);
+                tw.WriteLine("Fuck Me In The Asshole");
+                tw.Close();
+                Console.WriteLine("File Made!");
+            }catch(Exception e)
+            {
+                Console.Write(e);
+            }
+            Console.Read();
+        }
+    }
+}
---
+++ ipGoBrr/Program.cs
@@ -0,0 +1,40 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Net;
+using System.Net.Sockets;
+using System.Net.NetworkInformation;
+
+namespace ipGoBrr
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            Console.WriteLine("Local IP Address: " + GetIPAddress(Dns.GetHostName()));
+            Console.WriteLine("Google IP:" + GetIPAddress("google.com"));
+            Console.ReadLine();
+        }
+
+            static IEnumerable<string> GetAddresses()
+            {
+                var host = Dns.GetHostEntry(Dns.GetHostName());
+                return (from ip in host.AddressList where ip.AddressFamily == AddressFamily.InterNetwork select ip.ToString()).ToList();
+            }
+
+            static IPAddress GetIPAddress(string hostName)
+            {
+                Ping ping = new Ping();
+                var replay = ping.Send(hostName);
+
+                if (replay.Status == IPStatus.Success)
+                {
+                    return replay.Address;
+                }
+                return null;
+            }
+        }
+        
+    }
